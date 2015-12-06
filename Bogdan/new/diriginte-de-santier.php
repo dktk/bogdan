2aae2c36538e25b3cd35df9b110b2a520f920eb9
calin.tuns@gmail.com <calin.tuns@gmail.com>
2015-12-06 01:25:41 +0000

    diriginte de santier.

--- Bogdan/new/diriginte-de-santier.php
+++ Bogdan/new/diriginte-de-santier.php
@@ -6,7 +6,6 @@
 			"Advanced Design - Satu Mare",
 			"Advanced Design - Consultanta si excelenta in instalatii",
 
-			// todo: keywords
 			"diriginte de santier, dirigentie de santier, diriginte de santier satu mare, dirigintie de santier satu mare",
 
 			"diriginte-de-santier"
