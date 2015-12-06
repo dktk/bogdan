308e0c1aabb8179b0a3d651b9fbbf89939259fe9
calin.tuns@gmail.com <calin.tuns@gmail.com>
2015-12-06 04:10:34 +0000

    ,

--- Bogdan/new/templates/header.php
+++ Bogdan/new/templates/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" class="csstransforms csstransforms3d csstransitions"><head>
+<html lang="ro" class="csstransforms csstransforms3d csstransitions"><head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
